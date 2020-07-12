095e8d11761008c6cfd9a7c262e316e9c3f91ad1
Diego Evaldt Silveira <diegoesilveira@gmail.com>
2020-07-12 06:48:23 -0300

    Configurado o Lombok.

--- src/main/java/academy/devdojo/webflux/domain/Anime.java
+++ src/main/java/academy/devdojo/webflux/domain/Anime.java
@@ -3,12 +3,11 @@ package academy.devdojo.webflux.domain;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 
-import java.util.Objects;
-
-
 
 @Data
 @Builder
+@AllArgsConstructor
+@With
 public class Anime {
 
     @Id
@@ -16,48 +15,4 @@ public class Anime {
 
     private String name;
 
-    public Anime() {
-    }
-
-    public Anime(Integer id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Anime anime = (Anime) o;
-        return Objects.equals(id, anime.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Anime{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
